49668e9906a3d349ae6a75a1461387a7a23b6741
Mickael <61110922+MickaelMd@users.noreply.github.com>
2025-06-05 15:16:55 +0200

    image fixture

--- Fil_rouge/Symfony/GV_Doctrine/src/DataFixtures/SousCategorieFixture.php
+++ Fil_rouge/Symfony/GV_Doctrine/src/DataFixtures/SousCategorieFixture.php
@@ -18,33 +18,34 @@ class SousCategorieFixture extends Fixture implements DependentFixtureInterface
             ['nom' => 'Guitares Acoustiques', 'image' => 'image/souscategorie/guitares-acoustiques.jpg', 'categorie_reference' => 'categorie-guitares-bases', 'reference' => 'souscat-guitares-acoustiques'],
             ['nom' => 'Basses Electriques', 'image' => 'image/souscategorie/basses-electriques.jpg', 'categorie_reference' => 'categorie-guitares-bases', 'reference' => 'souscat-basses-electriques'],
 
-            ['nom' => 'Batteries Acoustiques', 'image' => 'image/souscategorie/default1.jpg', 'categorie_reference' => 'categorie-batteries-percussions', 'reference' => 'souscat-batteries-acoustiques'],
-            ['nom' => 'Batteries Electriques', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-batteries-percussions', 'reference' => 'souscat-batteries-electriques'],
+            ['nom' => 'Batteries Acoustiques', 'image' => 'image/souscategorie/batteries-acoustiques.jpg', 'categorie_reference' => 'categorie-batteries-percussions', 'reference' => 'souscat-batteries-acoustiques'],
+            ['nom' => 'Batteries Electriques', 'image' => 'image/souscategorie/batteries-electriques.jpg', 'categorie_reference' => 'categorie-batteries-percussions', 'reference' => 'souscat-batteries-electriques'],
 
-            ['nom' => 'Claviers Maîtres', 'image' => 'image/souscategorie/default1.jpg', 'categorie_reference' => 'categorie-pianos-claviers', 'reference' => 'souscat-claviers-maitres'],
-            ['nom' => 'Pianos de Scène', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-pianos-claviers', 'reference' => 'souscat-pianos-scene'],
+            ['nom' => 'Claviers Maîtres', 'image' => 'image/souscategorie/claviers-maitres.jpg', 'categorie_reference' => 'categorie-pianos-claviers', 'reference' => 'souscat-claviers-maitres'],
+            ['nom' => 'Pianos de Scène', 'image' => 'image/souscategorie/pianos-scene.jpg', 'categorie_reference' => 'categorie-pianos-claviers', 'reference' => 'souscat-pianos-scene'],
 
-            ['nom' => 'Interfaces Audio', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-studio-enregistrement', 'reference' => 'souscat-interfaces-audio'],
-            ['nom' => 'Microphones', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-studio-enregistrement', 'reference' => 'souscat-microphones'],
+            ['nom' => 'Interfaces Audio', 'image' => 'image/souscategorie/interfaces-audio.jpg', 'categorie_reference' => 'categorie-studio-enregistrement', 'reference' => 'souscat-interfaces-audio'],
+            ['nom' => 'Microphones', 'image' => 'image/souscategorie/microphones.jpg', 'categorie_reference' => 'categorie-studio-enregistrement', 'reference' => 'souscat-microphones'],
 
-            ['nom' => 'Saxophones', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-instruments-vent', 'reference' => 'souscat-saxophones'],
-            ['nom' => 'Trompettes', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-instruments-vent', 'reference' => 'souscat-trompettes'],
+            ['nom' => 'Saxophones', 'image' => 'image/souscategorie/saxophones.jpg', 'categorie_reference' => 'categorie-instruments-vent', 'reference' => 'souscat-saxophones'],
+            ['nom' => 'Trompettes', 'image' => 'image/souscategorie/trompettes.jpg', 'categorie_reference' => 'categorie-instruments-vent', 'reference' => 'souscat-trompettes'],
 
-            ['nom' => 'Violons Acoustiques', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-instruments-traditionnels', 'reference' => 'souscat-violons-acoustiques'],
-            ['nom' => 'Violons Electriques', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-instruments-traditionnels', 'reference' => 'souscat-violons-electriques'],
+            ['nom' => 'Violons Acoustiques', 'image' => 'image/souscategorie/violons-acoustiques.jpg', 'categorie_reference' => 'categorie-instruments-traditionnels', 'reference' => 'souscat-violons-acoustiques'],
+            ['nom' => 'Violons Electriques', 'image' => 'image/souscategorie/violons-electriques.jpg', 'categorie_reference' => 'categorie-instruments-traditionnels', 'reference' => 'souscat-violons-electriques'],
 
-            ['nom' => 'Câbles pour Instruments', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-cables-connectique', 'reference' => 'souscat-cables-instruments'],
-            ['nom' => 'Câbles pour Microphones', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-cables-connectique', 'reference' => 'souscat-cables-microphones'],
+            ['nom' => 'Câbles pour Instruments', 'image' => 'image/souscategorie/cables-instruments.jpg', 'categorie_reference' => 'categorie-cables-connectique', 'reference' => 'souscat-cables-instruments'],
+            ['nom' => 'Câbles pour Microphones', 'image' => 'image/souscategorie/cables-microphones.jpg', 'categorie_reference' => 'categorie-cables-connectique', 'reference' => 'souscat-cables-microphones'],
 
-            ['nom' => 'Casques Audio', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-accessoires', 'reference' => 'souscat-casques-audio'],
-            ['nom' => 'Bijoux pour Musiciens', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-accessoires', 'reference' => 'souscat-bijoux-musiciens'],
+            ['nom' => 'Casques Audio', 'image' => 'image/souscategorie/casques-audio.jpg', 'categorie_reference' => 'categorie-accessoires', 'reference' => 'souscat-casques-audio'],
+            ['nom' => 'Bijoux pour Musiciens', 'image' => 'image/souscategorie/bijoux-musiciens.jpg', 'categorie_reference' => 'categorie-accessoires', 'reference' => 'souscat-bijoux-musiciens'],
 
-            ['nom' => 'Séquenceurs et Studios Virtuels', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-logiciels-musicaux', 'reference' => 'souscat-sequenceurs-studios'],
-            ['nom' => 'Plug-Ins & Effets Audio', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-logiciels-musicaux', 'reference' => 'souscat-plugins-effets'],
+            ['nom' => 'Séquenceurs et Studios Virtuels', 'image' => 'image/souscategorie/sequenceurs-studios.jpg', 'categorie_reference' => 'categorie-logiciels-musicaux', 'reference' => 'souscat-sequenceurs-studios'],
+            ['nom' => 'Plug-Ins & Effets Audio', 'image' => 'image/souscategorie/plugins-effets.jpg', 'categorie_reference' => 'categorie-logiciels-musicaux', 'reference' => 'souscat-plugins-effets'],
 
-            ['nom' => 'Enceintes', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-sonorisation', 'reference' => 'souscat-enceintes'],
-            ['nom' => 'Accessoires Sonorisation', 'image' => 'image/souscategorie/default2.jpg', 'categorie_reference' => 'categorie-sonorisation', 'reference' => 'souscat-accessoires-sonorisation'],
+            ['nom' => 'Enceintes', 'image' => 'image/souscategorie/enceintes.jpg', 'categorie_reference' => 'categorie-sonorisation', 'reference' => 'souscat-enceintes'],
+            ['nom' => 'Accessoires Sonorisation', 'image' => 'image/souscategorie/accessoires-sonorisation.jpg', 'categorie_reference' => 'categorie-sonorisation', 'reference' => 'souscat-accessoires-sonorisation'],
         ];
+
 
 
         foreach ($sousCategoriesData as $data) {
